eb63df2f829090d2c312016cce6a54ac2f2afd03
K1ndbeetle <alx-s@bk.ru>
2022-11-22 23:06:28 +0300

    Task №2

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,22 @@ public class Main {
         } else {
             System.out.println("У вас неизвестная версия ОС");
         }
+
+        //task №2
+        System.out.println("Задача №2");
+        int clientDeviceYear = 2015;
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        if (clientOS < 0 || clientOS > 2) {
+            System.out.println("У вас неизвестная версия ОС");
+        }
     }
 }
