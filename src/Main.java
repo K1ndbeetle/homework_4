b8766db96a5f309469e9a2b421aaffa7de217d93
K1ndbeetle <alx-s@bk.ru>
2022-11-22 23:25:12 +0300

    Task №3

--- src/Main.java
+++ src/Main.java
@@ -28,5 +28,14 @@ public class Main {
         if (clientOS < 0 || clientOS > 2) {
             System.out.println("У вас неизвестная версия ОС");
         }
+
+        //task №3
+        System.out.println("Задача №3");
+        int year = 2021;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
     }
 }
